798b037f4c6b90fba1fb517a398b5c6501205922
cookiesikeri <ikbenezer@gmail.com>
2022-12-20 08:57:53 +0100

    rtyh

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -22,7 +22,7 @@
 @slot('footer')
 @component('mail::footer')
     <div style="">
-        Your digital banking password, PIN, OTP, debit card number or debit card PIN are personal details and should not be revealed to anyone. TranSave will never ask you to disclose any of these details via phone, mail, website or any other means. Please disregard any requests for your private data and report any suspicious activities to <span style="color: #3869d4">support@transave.com.ng</span> We are always available to answer your questions.
+        Your digital banking password, PIN, OTP, debit card number or debit card PIN are personal details and should not be revealed to anyone. Taheerexchange will never ask you to disclose any of these details via phone, mail, website or any other means. Please disregard any requests for your private data and report any suspicious activities to <span style="color: #3869d4">complaints@taheerexchange.com</span> We are always available to answer your questions.
     </div>
 @endcomponent
 @endslot
