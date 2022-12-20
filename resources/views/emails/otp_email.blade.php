d38a49b9b560a3e3e26ba25c3cf2f65fc77831e1
cookiesikeri <ikbenezer@gmail.com>
2022-12-20 19:04:11 +0100

    rtyh

--- resources/views/emails/otp_email.blade.php
+++ resources/views/emails/otp_email.blade.php
@@ -1,7 +1,8 @@
 @component('mail::message')
 Dear {{$name}}
 
-Verify your email to complete the sign up process {{$otp}},  Below is your OTP;
+Verify your email to complete the sign up process,  Below is your OTP;
+<strong> {{$otp}} </strong>
 
 If you did not request this, you can ignore this email or contact us at 09126397198
 thank you!
