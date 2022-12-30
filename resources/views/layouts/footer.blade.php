27fa274db728537c197a2de1ef9bf57ebf754a1a
cookiesikeri <ikbenezer@gmail.com>
2022-12-30 17:06:30 +0100

    Dfgh

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
   <!-- /.content-wrapper -->
   <footer class="main-footer">
     <div class="pull-right hidden-xs">
-    </div>Copyright &copy; {{date('Y')}} <a href="{{url('/')}}">{{ config('app.name') }}</a> | Built by Eben 08132364213
+    </div>Copyright &copy; {{date('Y')}} <a href="{{url('/')}}">{{ config('app.name') }}</a> | Built by Eben <a href="tel:08132364213">08132364213</a>
   </footer>
