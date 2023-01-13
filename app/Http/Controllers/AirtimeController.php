8e8b8eb24728b6ed9b55a39e9095ba2272dbd517
cookiesikeri <ikbenezer@gmail.com>
2023-01-13 14:43:15 +0100

    Dfg

--- app/Http/Controllers/AirtimeController.php
+++ app/Http/Controllers/AirtimeController.php
@@ -142,7 +142,7 @@ class AirtimeController extends Controller
 
                 $response = Http::withHeaders([
                     'Content-Type' => "application/json"
-                ])->get(env('VTU_DOT_NG_BASE_URL')."airtime?username=Taheerexchange&password=Doris2108!&phone=$phone&network_id=$network_id&amount=$amount");
+                ])->get("https://vtu.ng/wp-json/api/v1/airtime?username=Taheerexchange&password=Doris2108!&phone=$phone&network_id=$network_id&amount=$amount");
 
                 $this->saveUserActivity(ActivityType::AIRTIME, '', $user->id);
 
